f77507912d6c4f0d2be78238972b25111d11a672
tutoOmarclear <omar-ortiz@javeriana.edu.co>
2026-04-08 11:56:51 -0500

    feat: Enhance CartHasProducts question with wait condition and update CartTestRunner configuration

--- src/test/java/com/yourstore/automation/runners/CartTestRunner.java
+++ src/test/java/com/yourstore/automation/runners/CartTestRunner.java
@@ -6,12 +6,10 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.yourstore.automation")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.plugin.CucumberListener")
 public class CartTestRunner {
 }
