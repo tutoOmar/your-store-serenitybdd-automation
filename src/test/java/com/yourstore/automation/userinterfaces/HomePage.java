2f4980e06134696b9f274dcadc5414665f59493e
tutoOmarclear <omar-ortiz@javeriana.edu.co>
2026-04-08 11:10:34 -0500

    feat: Create HomePage class with target for adding first product to cart

---
+++ src/test/java/com/yourstore/automation/userinterfaces/HomePage.java
@@ -0,0 +1,12 @@
+package com.yourstore.automation.userinterfaces;
+
+import net.serenitybdd.screenplay.targets.Target;
+
+public class HomePage {
+
+    public static final Target ADD_TO_CART_FIRST_PRODUCT = Target.the("Add to cart button of first product")
+        .locatedBy("(//button[contains(@onclick,'cart.add')])[1]");
+    
+    private HomePage() {
+    }
+}
